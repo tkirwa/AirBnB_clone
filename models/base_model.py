8a385a36cc34f3b9c8b3dc73802b4339e9ad1b4d
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-05-12 10:48:53 +0300

    Store first object

--- models/base_model.py
+++ models/base_model.py
@@ -8,6 +8,7 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        storage.new(self)
 
     def __str__(self):
         class_name = self.__class__.__name__
