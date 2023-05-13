e0bd177dc03e0fe220e435a4ecc51292cafdc0af
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-05-13 10:54:45 +0300

    More classes - models/state.py, models/city.py, models/amenity.py, models/place.py, models/review.py, tests/

--- models/review.py
+++ models/review.py
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
-=======
-#!/usr/bin/python3
-"""
-Review class inheriting from BaseModel
-"""
->>>>>>> 4157f1142c53c8a5f10c3c863280090d635f4fc5
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-<<<<<<< HEAD
     """A class representing a review."""
 
     def __init__(self, *args, **kwargs):
@@ -18,10 +10,3 @@ class Review(BaseModel):
         self.place_id = ""
         self.user_id = ""
         self.text = ""
-=======
-    """Defining public class attributes
-    """
-    place_id = ""
-    user_id = ""
-    text = ""
->>>>>>> 4157f1142c53c8a5f10c3c863280090d635f4fc5
