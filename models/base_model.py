89fe7d8c05502ae5cdc238aed48028138c7e9ca0
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-05-14 00:23:06 +0300

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -7,16 +7,9 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Initializes public instance attributes."""
-        if kwargs:
-            del kwargs['__class__']
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the BaseModel instance."""
