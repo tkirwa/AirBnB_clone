4157f1142c53c8a5f10c3c863280090d635f4fc5
Delegat-is <kelvinjuma903@gmail.com>
2023-05-13 00:48:07 -0700

    update

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Class Amenity inheriting from BaseModel
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Defining public class attributes
+    """
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+State Class inheriting from BaseModel
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Defining public class attributes
+    """
+    name = ""
